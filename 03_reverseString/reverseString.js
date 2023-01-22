5cd03af4043b2b482bdb204e36f04c258d53e091
Carlos Batistela <cbhenry96@gmail.com>
2023-01-22 16:09:58 -0300

    Add function elements

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    var text_split = text.split('');
+    
+    var text_reverse = text_split.reverse();
+    
+    var text_join = text_reverse.join('');
+    
+    return text_join;
 };
 
 // Do not edit below this line
