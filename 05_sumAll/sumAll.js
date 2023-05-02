f0818f3de5d97dae4fb29bc86be56ede24039630
Carlos Batistela <cbhenry96@gmail.com>
2023-05-02 16:31:22 -0300

    Add function to sum all between

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || isNaN(start) || start < 1 || typeof end !== "number" || isNaN(end) || end < 1) {
+        return "ERROR";
+      }
 
+      if (start > end) {
+        [start, end] = [end, start];
+      }
+      let sum = 0;
+      for (let i = start; i <= end; i++) {
+        sum += i;
+      }
+      return sum;
 };
 
 // Do not edit below this line
