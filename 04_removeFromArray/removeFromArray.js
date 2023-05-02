a03ae6be16ecdc8c84c978dcc64bf461bf1f1787
Carlos Batistela <cbhenry96@gmail.com>
2023-05-02 16:04:53 -0300

    Add function to remove values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elementsToRemove) {
+    return arr.filter((element) => !elementsToRemove.includes(element))
 };
 
 // Do not edit below this line
